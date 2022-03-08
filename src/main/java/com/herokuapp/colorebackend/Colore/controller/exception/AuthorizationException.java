f7f6d8e9e0b4db2e4eca46f0aba5ff7945d5a0ea
Patricia Brandão <patriciabrandao7@gmail.com>
2022-03-08 19:08:07 -0300

    Inserindo controller/excepitions e DTO's

---
+++ src/main/java/com/herokuapp/colorebackend/Colore/controller/exception/AuthorizationException.java
@@ -0,0 +1,15 @@
+package com.herokuapp.colorebackend.Colore.controller.exception;
+
+public class AuthorizationException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public AuthorizationException(String msg) {
+		super(msg);
+	}
+	
+	public AuthorizationException(String msg, Throwable cause) {
+		super(msg, cause);
+	}
+
+}
