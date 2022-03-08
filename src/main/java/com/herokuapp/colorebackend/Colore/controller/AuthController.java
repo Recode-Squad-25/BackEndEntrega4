bdf0d8b4ea08bf003a2ff804b7ff4043497959a2
Patricia Brandão <patriciabrandao7@gmail.com>
2022-03-08 19:01:36 -0300

    Inserindo configs e controllers

---
+++ src/main/java/com/herokuapp/colorebackend/Colore/controller/AuthController.java
@@ -0,0 +1,43 @@
+package com.herokuapp.colorebackend.Colore.controller;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.colore.dto.EmailDTO;
+import br.com.colore.security.JWTUtil;
+import br.com.colore.security.UserSS;
+import br.com.colore.services.AuthService;
+import br.com.colore.services.UserService;
+
+@RestController
+@RequestMapping(value = "/auth")
+public class AuthController {
+
+	@Autowired
+	private JWTUtil jwtUtil;
+	
+	@Autowired
+	private AuthService service;
+	
+	@RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
+	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
+		UserSS user = UserService.authenticated();
+		String token = jwtUtil.generateToken(user.getUsername());
+		response.addHeader("Authorization", "Bearer " + token);
+		response.addHeader("access-control-expose-headers", "Authorization");
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value = "/forgot", method = RequestMethod.POST)
+	public ResponseEntity<Void> forgot(@Valid @RequestBody EmailDTO objDto) {
+		service.sendNewPassword(objDto.getEmail());
+		return ResponseEntity.noContent().build();
+	}
+}
